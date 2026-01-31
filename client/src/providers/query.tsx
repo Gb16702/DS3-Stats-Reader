00e570c215bbfa2df56b5610839fbb9179104511
Gb16702 <delgeoffrey1@gmail.com>
2026-01-31 13:52:03 +0100

    refactor(client): move tanstack query to providers folder

---
+++ client/src/providers/query.tsx
@@ -0,0 +1,24 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
+
+export function getContext() {
+  const queryClient = new QueryClient();
+  return { queryClient };
+}
+
+export function Provider({
+  children,
+  queryClient,
+}: {
+  children: React.ReactNode;
+  queryClient: QueryClient;
+}) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
+
+export const Devtools = {
+  name: "Tanstack Query",
+  render: <ReactQueryDevtoolsPanel />,
+};
